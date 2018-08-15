68f9c2364606a9f6c041a36266cd12960bdb1a6e
gmy <296778322@qq.com>
2018-08-15 22:48:43 +0800

    <Update firmware(v1.0.2) bridge(1.0.0)>

--- verno.js
+++ verno.js
@@ -1,5 +1,5 @@
 const version_name = {
-    firmware:"firmware-upgrade-v1.0.1.bin",
+    firmware:"firmware-upgrade-v1.0.2.bin",
     bridge_linux32:"justsafe-bridge_1.0.0_i386.deb",
     bridge_linux64:"justsafe-bridge_1.0.0_amd64.deb",
     bridge_mac:"justsafe-bridge-1.0.0-win32-install.exe",
