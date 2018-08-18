d160d34b07cd3768e611a6dc4fd5a4dc908df679
gmy <296778322@qq.com>
2018-08-18 10:27:00 +0800

    update config_signed.bin

--- verno.js
+++ verno.js
@@ -1,6 +1,6 @@
 const version_name = {
     firmware:"firmware-upgrade-v1.0.3.bin",
-    hash:"",
+    hash:"7bf0103db7fe026ab9233b4dddbf1b2a5b266cbecd885157453d64a268188f4b",
     bridge_linux64:"justsafe-bridge_1.0.0_amd64.deb",
     bridge_mac:"justsafe-bridge-1.0.0.pkg",
     bridge_win:"justsafe-bridge-1.0.0-win32-install.exe",
